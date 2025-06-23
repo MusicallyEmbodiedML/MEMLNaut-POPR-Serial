5d0f18802eda5001efe25ea628e06d80f472c9a8
Andrea Martelloni <a.martelloni@sussex.ac.uk>
2025-06-23 12:11:59 +1000

    Integrated MIDI into POPR

--- MEMLNaut-PAF-IML.ino
+++ MEMLNaut-PAF-IML.ino
@@ -96,7 +96,7 @@ void setup()
     midi_interf->SetMIDISendChannel(1);
     Serial.println("MIDI setup complete.");
     if (midi_interf) {
-        midi_interf->SetNoteCallback([RLInterface] (bool noteon, uint8_t note_number, uint8_t vel_value) {
+        midi_interf->SetNoteCallback([interface] (bool noteon, uint8_t note_number, uint8_t vel_value) {
         if (noteon) {
             uint8_t midimsg[2] = {note_number, vel_value };
             queue_try_add(&audio_app->qMIDINoteOn, &midimsg);
